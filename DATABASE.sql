c673b3f26c39aa2314c4710d533f06fbbc034ee8
Nanteza Angellah <142222757+Angel199610@users.noreply.github.com>
2024-03-11 16:40:33 +0300

    DBMS_Test

---
+++ DATABASE.sql
@@ -0,0 +1,16 @@
+--CREATE TABLE students (
+--Reg_no INT,
+--First_Name VARCHAR (255),
+--Last_Name VARCHAR (255),
+--Course VARCHAR (100),
+--Gender VARCHAR (100),
+--D_O_B INT);
+
+
+INSERT INTO students (Reg_no, First_Name, Last_Name, Course, Gender, D_O_B)
+VALUES (1/2016, 'John', 'Mutuku', 'DCS', 'Male', 13/6/1990),
+(2/2016, 'Steve', 'KipoKorir', 'DCS', 'Male', 13/3/1985),
+(3/2016, 'Suzan', 'Mutua', 'CIT', 'Female', 13/6/1986),
+(4/2016, 'Steve', 'Kingori', 'DBIT', 'Male', 13/6/1978);
+SELECT * FROM students; 
+
